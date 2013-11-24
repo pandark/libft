c6aac50bd93e0383c5e77bd9e61d45e42cdd5bb2
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-24 23:42:34 +0100

    bzerotest

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,14 +6,17 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:38:43 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/22 16:54:41 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/24 23:42:05 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 #include <string.h>
 
 void	ft_bzero(void *s, size_t n)
 {
+	write(1, "ft_bzero", 8);
 	(void)s;
 	(void)n;
 }
