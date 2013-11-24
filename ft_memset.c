c37c335ac09683d73ce73852cf725d6da9d52f4d
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-24 22:34:06 +0100

    ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,17 +6,26 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 20:56:12 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/22 17:00:32 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/24 22:33:26 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 #include <string.h>
 
-void	*ft_memset(void *b, int c, size_t len)
+void    *ft_memset(void *b, int c, size_t len)
 {
-	(void)b;
-	(void)c;
-	(void)len;
-	return (0);
+	unsigned char   *cur;
+
+	write(1, "ft_memset", 9);
+	cur = (unsigned char *)b;
+	while (len > 0)
+	{
+		*cur = (unsigned char)c;
+		cur++;
+		len--;
+	}
+	return b;
 }
 
