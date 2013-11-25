36b9cf3739febbda920503f7a49aff647354de1f
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-25 17:58:00 +0100

    pointeur au lieu de valeur

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 18:23:12 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/25 00:31:15 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/25 17:57:33 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_putstr_fd(char const *s, int fd)
 	i = 0;
 	while (s + i != NULL && s[i] != '\0')
 	{
-		ft_putchar_fd(s + i, fd);
+		ft_putchar_fd(s[i], fd);
 		i++;
 	}
 }
