275d327cb43cfabdd376ac541da96dbc84dd5854
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-26 21:05:25 +0100

    essaie

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:38:49 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/26 19:12:57 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/26 21:05:09 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	len2;
 
 	len1 = strlen(s1);
-	len2 = strlen(s2);
 	n = (len1 < n) ? len1 : n;
+	len2 = strlen(s2);
 	cur = (char *)s1;
 	if (*s2 == '\0')
 	{
 		return (cur);
 	}
-	while (n >= len2 && ft_memchr(cur, *s2, n - len2) != NULL)
+	while (n > 0 && ft_memchr(cur, *s2, n - len2) != NULL)
 	{
 		if (ft_memcmp(cur, s2, len2) == 0)
 		{
