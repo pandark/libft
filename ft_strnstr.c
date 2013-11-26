2cdc939d48183fdff6fe6103bcac3b7a1cf99851
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-26 15:20:16 +0100

    ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:38:49 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/25 21:24:08 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/26 15:18:36 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,19 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 {
 	write(1, "ft_strnstr", 10);
 	char	*c1;
-	size_t		len_s2;
-	size_t		len_cmp;
+	size_t	len_cmp;
 
-	len_s2 = ft_strlen(s2);
-	len_cmp = ft_strlen(s1) - len_s2;
-	len_cmp = (len_cmp <= n) ? len_cmp : n;
+	len_cmp = ft_strlen(s1) - ft_strlen(s2);
+	len_cmp = (n < ft_strlen(s1)) ? n - ft_strlen(s2) : len_cmp;
 	c1 = (char *)s1;
-	while (ft_strchr(c1, *s2) != NULL)
+	while (ft_memchr(c1, *s2, len_cmp) != NULL)
 	{
-		if (ft_memcmp(c1, s2, len_s2) == 0)
+		if (ft_memcmp(c1, s2, ft_strlen(s2)) == 0)
 		{
 			return (c1);
 		}
 		c1++;
+		len_cmp--;
 	}
 	return (NULL);
 }
