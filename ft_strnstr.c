101e5c40d397a5fbda2ff23fd390fb00a35e07ea
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-26 21:35:37 +0100

    manquait ft_

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:38:49 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/26 21:05:09 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/26 21:35:15 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 	size_t	len1;
 	size_t	len2;
 
-	len1 = strlen(s1);
+	len1 = ft_strlen(s1);
 	n = (len1 < n) ? len1 : n;
-	len2 = strlen(s2);
+	len2 = ft_strlen(s2);
 	cur = (char *)s1;
 	if (*s2 == '\0')
 	{
