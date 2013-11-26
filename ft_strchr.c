b7485586c4b45f4f7786ad091e8f980ff35c18f8
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-26 14:31:23 +0100

    mauvaise fonction strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 16:38:48 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/25 19:26:34 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/26 14:30:38 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,17 @@
 char	*ft_strchr(const char *s, int c)
 {
 	write(1, "ft_strchr", 10);
-	int		cur;
 	char	*str;
 
 	str = (char *)s;
-	cur = 0;
-	while (str[cur] != c)
+	while (*str != c)
 	{
-		if (str[cur] == '\0')
+		if (*str == '\0')
 		{
-			return (NULL);
+			return ((*str == '\0') ? str : NULL);
 		}
-		cur++;
+		str++;
 	}
-	return (str + cur);
+	return (str);
 }
 
