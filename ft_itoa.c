f2bdcd7f7daf659d90fb3f703f35e3311635c2c5
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-29 16:32:57 +0100

    essaie itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,18 +6,48 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 17:43:41 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/24 23:07:26 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/29 16:32:32 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
 #include <stdlib.h>
 
-char	*ft_itoa(int n)
+static void		int_lengths(int n, size_t *len, int *weight)
 {
-	write(1, "ft_itoa", 7);
-	(void)n;
-	return (0);
+
+	*len = (n < 0) ? 1 : 0;
+	n = (n < 0) ? -n : n;
+	*weight = 1;
+	while (n / *weight > 9)
+	{
+		*weight *= 10;
+		*len += 1;
+	}
+}
+
+char			*ft_itoa(int n)
+{
+	size_t	len;
+	int		weight;
+	size_t	cur;
+	char	*str;
+
+	int_lengths(n, &len, &weight);
+	str = (char *)malloc(sizeof(str) * len);
+	if (str == NULL)
+		return (NULL);
+	cur = 0;
+	if (n < 0)
+	{
+		str[cur] = '-';
+		cur++;
+		n = -n;
+	}
+	while (weight > 1)
+	{
+		str[cur++] = n / weight % 10;
+		weight /= 10;
+	}
+	return (str);
 }
 
