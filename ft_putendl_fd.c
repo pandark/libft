3a24fdef158e6e7e484650cfcbcf5dc7ff2a63d7
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-30 03:46:24 +0100

    endline instead of endstring

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 18:27:45 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/25 00:26:31 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/30 03:45:21 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@
 void	ft_putendl_fd(char const *s, int fd)
 {
 	ft_putstr_fd(s, fd);
-	ft_putchar_fd('\0', fd);
+	ft_putchar_fd('\n', fd);
 }
 
