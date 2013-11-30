cf311f79dc7c16a6c8b4ee24e3e7709d974c3d62
Adrien Pachkoff <apachkof@student.42.fr>
2013-11-30 03:42:19 +0100

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,19 +6,22 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 17:43:41 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/29 16:32:32 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/11/30 03:41:38 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-static void		int_lengths(int n, size_t *len, int *weight)
+static void		lengths(int n, size_t *len, int *weight)
 {
-
-	*len = (n < 0) ? 1 : 0;
-	n = (n < 0) ? -n : n;
+	*len = 1;
+	if (n >= 0)
+	{
+		*len = 0;
+		n = -n;
+	}
 	*weight = 1;
-	while (n / *weight > 9)
+	while (n / *weight < -9)
 	{
 		*weight *= 10;
 		*len += 1;
@@ -27,13 +30,13 @@ static void		int_lengths(int n, size_t *len, int *weight)
 
 char			*ft_itoa(int n)
 {
-	size_t	len;
-	int		weight;
-	size_t	cur;
-	char	*str;
+	size_t		len;
+	int			weight;
+	size_t		cur;
+	char		*str;
 
-	int_lengths(n, &len, &weight);
-	str = (char *)malloc(sizeof(str) * len);
+	lengths(n, &len, &weight);
+	str = (char *)malloc(sizeof(str) * (len + 1));
 	if (str == NULL)
 		return (NULL);
 	cur = 0;
@@ -41,13 +44,15 @@ char			*ft_itoa(int n)
 	{
 		str[cur] = '-';
 		cur++;
-		n = -n;
 	}
-	while (weight > 1)
+	if (n > 0)
+		n = -n;
+	while (weight >= 1)
 	{
-		str[cur++] = n / weight % 10;
+		str[cur++] = -(n / weight % 10) + 48;
 		weight /= 10;
 	}
+	str[cur] = '\0';
 	return (str);
 }
 
