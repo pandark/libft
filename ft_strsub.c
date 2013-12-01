68ff5f5ec4acd0f6ac55dbe26593795df63b58af
Adrien Pachkoff <apachkof@student.42.fr>
2013-12-01 21:26:54 +0100

    correction strsub

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 17:31:12 by apachkof          #+#    #+#             */
-/*   Updated: 2013/12/01 20:58:52 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/12/01 21:22:14 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,17 @@
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*str;
+	size_t	cur;
 
-	if (start > ft_strlen((const char *)s))
-		return ("");
-	len = (len <= ft_strlen((const char *)(&s[start]))) ? len : ft_strlen((const char *)(&s[start]));
 	str = (char *)malloc(sizeof(*str) * (len + 1));
 	if (str == NULL)
 		return (NULL);
-	ft_strncpy(str, (const char *)(&s[start]), len);
+	cur = 0;
+	while (cur < len)
+	{
+		str[cur] = s[start + cur];
+		cur++;
+	}
+	str[cur] = '\0';
 	return (str);
 }
