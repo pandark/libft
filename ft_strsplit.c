73938546148ddc3d3f0ec3fc1f83e131d19dd2c7
Adrien Pachkoff <apachkof@student.42.fr>
2013-12-01 22:50:50 +0100

    strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,67 +6,68 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 17:41:45 by apachkof          #+#    #+#             */
-/*   Updated: 2013/12/01 19:37:26 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/12/01 22:50:17 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-static size_t	ft_cnt_parts(const char *s, char c)
+static int		ft_cnt_parts(const char *s, char c)
 {
-	size_t		cnt;
+	int		cnt;
+	int		in_substring;
 
-	if (c == '\0')
-		return (1);
-	cnt = 1;
-	while (*s == c)
-		s++;
-	s++;
+	in_substring = 0;
+	cnt = 0;
 	while (*s != '\0')
 	{
-		while (*s == c && *(s-1) == c)
-			s++;
-		if (*s == c && *(s + 1) != '\0')
+		if (in_substring == 1 && *s == c)
+			in_substring = 0;
+		if (in_substring == 0 && *s != c)
+		{
+			in_substring = 1;
 			cnt++;
+		}
 		s++;
 	}
 	return (cnt);
 }
 
-static void		ft_banana(char **t, const char *s, char c)
+static int		ft_wlen(const char *s, char c)
 {
-	size_t		start;
-	size_t		end;
-	size_t		i;
+	int		len;
 
-	start = 0;
-	end = 0;
-	i = 0;
-	while (s[start] != '\0')
+	len = 0;
+	while (*s != c && *s != '\0')
 	{
-		end = start;
-		while (s[end] != '\0' && s[end] != c)
-			end++;
-		if (end - start > 1)
-		{
-			t[i] = (char *)malloc(sizeof(char) * (end - start));
-			i++;
-		}
-		start = end + 1;
+		len++;
+		s++;
 	}
-	t[i] = ft_strnew(0);
-	if (i == 0)
-		t[1] = ft_strnew(0);
+	return (len);
 }
 
 char			**ft_strsplit(char const *s, char c)
 {
-	char		**t;
-	size_t		len;
+	char	**t;
+	int		nb_word;
+	int		index;
 
-	len = ft_cnt_parts((const char *)s, c);
-	t = (char **)malloc(sizeof(char *) * (len + 1));
-	ft_banana(t, s, c);
+	index = 0;
+	nb_word = ft_cnt_parts((const char *)s, c);
+	t = (char **)malloc(sizeof(*t) * (ft_cnt_parts((const char *)s, c) + 1));
+	if (t == NULL)
+		return (NULL);
+	while (nb_word--)
+	{
+		while (*s == c && *s != '\0')
+			s++;
+		t[index] = ft_strsub((const char *)s, 0, ft_wlen((const char *)s, c));
+		if (t[index] == NULL)
+			return (NULL);
+		s = s + ft_wlen(s, c);
+		index++;
+	}
+	t[index] = NULL;
 	return (t);
 }
