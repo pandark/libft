3c8adcd68471c5eb86d2bc969dd376b78e01827c
Adrien Pachkoff <apachkof@student.42.fr>
2013-12-01 23:38:32 +0100

    lastmin

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: apachkof <apachkof@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/19 14:05:55 by apachkof          #+#    #+#             */
-/*   Updated: 2013/11/25 21:30:08 by apachkof         ###   ########.fr       */
+/*   Updated: 2013/12/01 23:38:13 by apachkof         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ void	ft_strdel(char **as);
 void	ft_strclr(char *s);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
-char * ft_strmap(char const *s, char (*f)(char));
+char	*ft_strmap(char const *s, char (*f)(char));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
@@ -81,5 +81,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void const *content, size_t content_size);
 
 #endif
